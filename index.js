50764aaf96bf964cc9d15b217bb11811e138c3d9
Sanghee Kim <sangheestyle@gmail.com>
2015-02-10 23:58:23 -0700

    get request with param then response with the param
    This is a kind of example for searching by given param
    i.e, regex for git log...
    
    For example:
    
    $ curl -d '{"regex":"SetTag"}' -H "Content-Type: application/json" \
    http://localhost/search/git
    
    will return
    
    {"regex":"SetTag",
     "repos":[{"name":"repo1",
               "commits":[{"sha":"1a2e3","patch":"bla~"},
                          {"sha":"2klj3","patch":"blam~"}]},
              {"name":"repo2",
               "commits":[{"sha":"4a2e3","patch":"bla~"},
                          {"sha":"7klj3","patch":"blam~"}]}]
    }
    
    This is just a case for searching but I think we can understand
    the basic mechanism of query for searching something.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 // Constants
 var PORT = 80;
 
 // App
 var app = express();
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
   res.json({message: "Hey! You are very welcome."
   });
@@ -16,6 +19,23 @@ app.get('/who_am_i', function (req, res) {
 	  ,favorites: ["coffee"]
           ,int_num: 5
           ,float_num: 5.12
+  });
+});
+
+// return commit lists including given regex pattern
+//
+// Expected request format:
+// $ curl -d '{"regex":"settag"}' -H "Content-Type: application/json" \
+//   http://localhost/search/git
+app.post('/search/git', function (req, res) {
+  regex = req.body["regex"];
+  res.json({regex: regex
+           ,repos: [{name: "repo1"
+                    ,commits: [{sha:"1a2e3", patch: "bla~"}
+                              ,{sha:"2klj3", patch: "blam~"}]}
+                   ,{name: "repo2"
+                    ,commits: [{sha:"4a2e3", patch: "bla~"}
+                              ,{sha:"7klj3", patch: "blam~"}]}]
   });
 });
 

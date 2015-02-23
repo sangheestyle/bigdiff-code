fccd58b7eede2d209ae9227cfaad9c772accda50
Brennan-M <brmc3887@colorado.edu>
2015-02-23 15:15:08 -0700

    More useful sample data if we use this time period. Otherwise, filtered = 0.

--- examples/searchRepos.js
+++ examples/searchRepos.js
@@ -2,8 +2,8 @@ var github = require('../lib/github');
 
 var params = {
   query: 'android in:AndroidManifest.xml language:java',
-  start: '2015-01-01',
-  end: '2015-02-01',
+  start: '2013-01-01',
+  end: '2013-02-01',
   client: github.authClient(id, password)
 };
 

0e07719a63eafda943c33c525f92c9ca9158679a
Sanghee Kim <sangheestyle@gmail.com>
2015-03-09 23:44:54 -0600

    Want to clone git repos which have default_branch as master
    If git's default_branch is not 'master', it makes errors during
    cloning. It's not really serious error but if we don't clone it,
    life gets easier in terms of managing and searching git.

--- examples/filterAndClone.js
+++ examples/filterAndClone.js
@@ -4,8 +4,10 @@ var git = require('../lib/git');
 
 var params = { url: "mongodb://localhost:27017/muse"
              , collection: "repo"
-             , query: {watchers: {$gte: 5}, size: {$lte: 20480}}
-             , fields: {full_name: 1, clone_url: 1, _id: 0}
+             , query: { watchers: { $gte: 5}
+                      , size: {$lte: 20480}
+                      , default_branch: "master"}
+             , fields: { full_name: 1, clone_url: 1, _id: 0}
 };
 var urlNames = {
   root: '/home/sanghee/muse_git_repo/',

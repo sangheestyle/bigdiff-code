c9756b206c275ff176ca0f4e04b7c719aa6d6534
Sanghee Kim <sangheestyle@gmail.com>
2015-03-10 12:22:49 -0600

    size of some repos which have setTag bug is over than 30MB

--- examples/filterAndClone.js
+++ examples/filterAndClone.js
@@ -5,7 +5,7 @@ var git = require('../lib/git');
 var params = { url: "mongodb://localhost:27017/muse"
              , collection: "repo"
              , query: { watchers: { $gte: 5}
-                      , size: {$lte: 20480}
+                      , size: {$lte: 40960}
                       , default_branch: "master"}
              , fields: { full_name: 1, clone_url: 1, _id: 0}
 };

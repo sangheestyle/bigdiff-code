b035d5912b341e4c39f8f3289f08ef0926ef6903
Sanghee Kim <sangheestyle@gmail.com>
2015-03-12 02:34:24 -0600

    RFT: remove some redundunt code for search repo(s)

--- examples/searchRepos.js
+++ examples/searchRepos.js
@@ -3,11 +3,19 @@ var config = require('../config.json');
 
 var id = config.github_id;
 var password = config.github_password;
-var params = {
-  query: 'android in:name,description,readme',
-  start: '2011-01-01',
-  end: '2015-03-01',
-  client: github.authClient(id, password)
-};
+var params = { query: 'android in:name,description,readme'
+             , start: '2015-01-15'
+             , end: '2015-01-20'
+             , per_page: 100
+             , client: github.authClient(id, password)
+             , config: config
+             };
 
-github.searchRepos(params);
+github.searchRepos(params, function(err) {
+  if (err) {
+    console.log('FAILED: Search Repos.');
+    console.log(err);
+  } else {
+    console.log('DONE: Search Repos.');
+  }
+});

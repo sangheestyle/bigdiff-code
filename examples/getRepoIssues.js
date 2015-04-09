15f3db167f72531b2006847d6802ccd7dff287a2
Brennan-M <brmc3887@colorado.edu>
2015-04-08 22:25:10 -0600

    github.getRepoIssues updated for MongoDB
    We now have the functionality to gather issues for a specified
    git repository.
    
    TODO: Be able to enter in the local_repo_root
    directory and iterate through it to call github.getRepoIssues for each
    repository cloned.

--- examples/getRepoIssues.js
+++ examples/getRepoIssues.js
@@ -1,9 +1,16 @@
 var github = require('../lib/github');
+var config = require('../config.json');
 
-var username = 'username';
-var password = 'password';
-var client = github.authClient(username, password);
+var id = config.github_id;
+var password = config.github_password;
+var params = { per_page: 100
+			 , state: 'open'
+             , client: github.authClient(id, password)
+             , config: config
+             };
 
-repo = 'github/android';
-params = {client: client, repo: repo};
 github.getRepoIssues(params);
+
+
+
+

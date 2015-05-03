0cfd90134e0f01574e4dacb58dabdf972c99a319
Sanghee Kim <sangheestyle@gmail.com>
2015-05-03 01:55:47 +0000

    simply export data from mongo to a csv file
    You can simply export data from mongo to a csv file by
    the following command:
    
    $ mongo test export_csv.js | tee out.csv

--- examples/export_csv.js
+++ examples/export_csv.js
@@ -1,6 +1,6 @@
 conn = new Mongo();
 db = conn.getDB('muse');
-print("full_name");
+print("full_name,size,forks,watchers,created_at,updated_at");
 db.repo.find().forEach(function(r){
   print(r.full_name + "," + r.size + "," + r.forks + "," + r.watchers + "," + r.created_at + "," +  r.updated_at);
 });

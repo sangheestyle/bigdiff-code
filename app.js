90caa7f8178a1cdc915af2a8f89825076c1f38a8
Sanghee Kim <sangheestyle@gmail.com>
2015-06-19 20:40:53 -0600

    Do clone repos periodically after filtering repo information on db
    On everyday at 00:00:01am, it starts to do filter limited repos
    on db by criteria and clone them.
    
    TODO: If a repo is already cloned, it just skip without error. But,
    I think the right way to do is that it would better to git pull if
    there already has cloned one.

--- app.js
+++ app.js
@@ -237,3 +237,37 @@ var jobSearchRepos = new CronJob({
   timeZone: 'America/Denver'
 });
 jobSearchRepos.start();
+
+/*
+ * JOB: clone repos after filter
+ * Runs everyday (Sunday through Saturday)
+ * at 00:00:01 AM.
+ */
+
+var jobCloneRepos = new CronJob({
+  cronTime: '01 00 00 * * 0-6',
+  onTick: function() {
+    var params = { url: config.mongo_url
+                 , collection: config.mongo_collection
+                 , query: { watchers: { $gte: 5}
+                          , size: {$lte: 40960}
+                          , default_branch: "master"
+                          , "private": false}
+                 , fields: { full_name: 1, clone_url: 1, _id: 0}
+    };
+    var urlNames = {
+      root: config.local_repo_root,
+      list: []
+    };
+
+    mongo.find(params, function(err, items) {
+      console.log('=== Starting clone repos for ' + items.length
+                  + ' items');
+      urlNames.list = items;
+      git.multipleClone(urlNames);
+    });
+  },
+  start: false,
+  timeZone: 'America/Denver'
+});
+jobCloneRepos.start();
